44a953fc19fa97856a1e6b28355f039bd429f39e
LauferVA <153957763+LauferVA@users.noreply.github.com>
2024-04-07 20:57:32 -0400

    Update eFetchParallel.py
    returns Xml or other data unformatted - formatting in process eFetchOutput.py now

--- eFetchParallel.py
+++ eFetchParallel.py
@@ -1,7 +1,6 @@
 import json
 import pandas as pd
 from Bio import Entrez
-from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 import time
 import os
@@ -24,14 +23,9 @@ logging.basicConfig(level=logging.INFO,
                     handlers=[logging.FileHandler(log_file_path),
                               logging.StreamHandler()])
 
-print('\n\n'
+print('\n\n')
 
 def printAndLog(message, level="info"):
-    """
-    Prints and logs a message at the specified level.
-    :param message: The message to print and log.
-    :param level: The logging level ('info', 'warning', 'error').
-    """
     print(message)  # Print to console
     if level.lower() == "info":
         logging.info(message)
@@ -39,8 +33,6 @@ def printAndLog(message, level="info"):
         logging.warning(message)
     elif level.lower() == "error":
         logging.error(message)
-    else:
-        logging.info(message)  # Default to info level if unspecified/incorrect level
 
 # Override the default HTTPS context
 def create_https_context():
@@ -51,33 +43,28 @@ ssl._create_default_https_context = create_https_context
 
 def handle_http_errors(func):
     def wrapper(*args, **kwargs):
-        retries = 3  # Number of retries for IncompleteRead
+        retries = 4
+        delay = 1.5  # Initial delay in seconds
         for attempt in range(retries):
             try:
                 return func(*args, **kwargs)
             except HTTPError as e:
-                printAndLog(f"HTTP error occurred: {e}", "error")
-                if e.code == 400:
-                    printAndLog("Bad request. Please check the parameters or request syntax.", "error")
-                    break  # No retry for client errors
-                elif e.code == 429:
-                    printAndLog("Too many requests. Sleeping for 10 seconds.", "warning")
-                    time.sleep(10)  # Retry after waiting
-                elif 500 <= e.code < 600:
-                    printAndLog("Server error. Retrying the request.", "warning")
-                    if attempt < retries - 1:
-                        time.sleep(2 ** (attempt + 1))  # Exponential backoff for server errors
-                    else:
-                        printAndLog("Final attempt failed.", "error")
-                        raise
+                if e.code == 429:
+                    printAndLog(f"429 Too Many Requests: Retrying in {delay} seconds...", "warning")
+                    time.sleep(delay)
+                    delay *= 2  # Double the delay for the next attempt
                 else:
-                    raise
+                    printAndLog(f"HTTP error occurred: {e}", "error")
+                    if e.code in [400, 500, 502, 503, 504]:
+                        time.sleep(delay)  # Wait before retrying
+                        delay *= 2  # Increase delay for the next attempt
+                    else:
+                        break  # Don't retry for other HTTP errors
             except IncompleteRead as e:
-                # Handle IncompleteRead separately to potentially retry
                 printAndLog(f"Incomplete read error: {e}, bytes read: {e.partial}", "warning")
                 if attempt < retries - 1:
                     printAndLog(f"Retrying due to incomplete read... Attempt {attempt + 1} of {retries}", "info")
-                    time.sleep(2 ** (attempt + 1))  # Exponential backoff
+                    time.sleep(delay)
                 else:
                     printAndLog("Final attempt failed due to incomplete read.", "error")
                     raise
@@ -109,19 +96,23 @@ def read_primary_ids(json_file_path):
     return data
 
 @handle_http_errors
-def fetch_metadata_for_batch(db, batch_ids, index, total_batches):
+def fetch_metadata_for_batch(db, batch_ids, index, total_batches, output_dir):
     try:
         # Attempt fetching with the original batch size
         records = Entrez.efetch(db=db, id=','.join(batch_ids), rettype="xml").read()
-        return records
+        if isinstance(records, bytes):
+            records = records.decode('utf-8')  # Decode bytes to string using utf-8 or appropriate encoding
+        with open(os.path.join(output_dir, f'batch_{index+1}.xml'), 'w') as file:
+            file.write(records)
+        return f"Batch {index+1} saved."
     except HTTPError as e:
         if e.code == 400:
             # If Bad Request, attempt to handle by splitting the batch
             mid = len(batch_ids) // 2
             if mid > 0:
                 printAndLog(f"Splitting batch {index+1}/{total_batches} due to HTTP Error 400.", "warning")
-                first_half = fetch_metadata_for_batch(db, batch_ids[:mid], index, total_batches)
-                second_half = fetch_metadata_for_batch(db, batch_ids[mid:], index, total_batches)
+                first_half = fetch_metadata_for_batch(db, batch_ids[:mid], index, total_batches, output_dir)
+                second_half = fetch_metadata_for_batch(db, batch_ids[mid:], index, total_batches, output_dir)
                 return first_half + second_half
             else:
                 printAndLog(f"Failed to fetch batch {index+1}/{total_batches}: {e}", "error")
@@ -129,7 +120,7 @@ def fetch_metadata_for_batch(db, batch_ids, index, total_batches):
         else:
             raise
 
-def parallel_fetch_metadata(primary_ids, db, batch_size=500):
+def parallel_fetch_metadata(primary_ids, db, batch_size, output_dir):
     total_records = len(primary_ids)
     total_batches = (total_records + batch_size - 1) // batch_size  # Ceiling division
     printAndLog(f"Total number of records to process: {total_records}\n")
@@ -137,9 +128,10 @@ def parallel_fetch_metadata(primary_ids, db, batch_size=500):
     with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
         futures = []
         for index, start in enumerate(range(0, len(primary_ids), batch_size)):  # Index usage
+            time.sleep(3)
             end = min(start + batch_size, len(primary_ids))
             batch_ids = primary_ids[start:end]
-            futures.append(executor.submit(fetch_metadata_for_batch, db, batch_ids, index, total_batches)) # Pass index and total_batches to the fetch_metadata_for_batch()
+            futures.append(executor.submit(fetch_metadata_for_batch, db, batch_ids, index, total_batches, output_dir)) # Pass index and total_batches to the fetch_metadata_for_batch()
         
         results = []
         for future in concurrent.futures.as_completed(futures):
@@ -148,61 +140,23 @@ def parallel_fetch_metadata(primary_ids, db, batch_size=500):
                 results.append(result)
     return results
 
-def flatten_xml(element, path="", flat_dict=None):
-    if flat_dict is None:
-        flat_dict = {}
-    if len(element.find_all(recursive=False)) > 0:
-        for child in element.find_all(recursive=False):
-            child_path = f"{path}/{child.name}" if path else child.name
-            flatten_xml(child, path=child_path, flat_dict=flat_dict)
-    else:
-        flat_dict[path] = element.text.strip()
-    return flat_dict
-
-def parse_and_flatten_xml(xml_records):
-    all_records = []
-    for record in xml_records:
-        try:
-            soup = BeautifulSoup(record, 'lxml')            # First attempt to parse with 'lxml'
-        except Exception as lxml_error:
-            printAndLog(f"lxml parsing failed: {lxml_error}, trying 'html5lib'", "warning")
-            try:
-                soup = BeautifulSoup(record, 'html5lib')                # Then fall back to 'html5lib' if 'lxml' fails
-            except Exception as html5lib_error:
-                printAndLog(f"html5lib parsing failed: {html5lib_error}, falling back to 'html.parser'", "warning")
-                soup = BeautifulSoup(record, 'html.parser')                # Last resort: use Python’s built-in HTML parser
-        flat_dict = flatten_xml(soup)
-        all_records.append(flat_dict)
-    return all_records
-
-def write_to_dataframe(records, output_file):
-    df = pd.DataFrame(records)
-    common_prefix = os.path.commonprefix(df.columns.tolist())
-    df.rename(columns=lambda x: x.replace(common_prefix, '').replace('/', '_'), inplace=True)
-    df.dropna(thresh=len(df) * 0.95, axis=1, inplace=True)
-    directory = os.path.dirname(output_file)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    df.to_csv(output_file, index=False, sep='\t')
-
 def main():
     start_time = datetime.now()
     printAndLog(f"Script started at {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    db = 'bioproject'
+    db = 'sra'
     credentialFile = '../inputFiles/Auth/EntrezCredentials.json'
     file_path_prefix = '/data/user/vlaufer/DeathStar'
     json_file_path = f"{file_path_prefix}/Data/esearch/{db}/{db}_Hs_Ids.json"
-    output_file = f"{file_path_prefix}/Data/efetch/{db}/{db}_hS_metadata.tsv"
-    #### start timer
+    output_dir = f"{file_path_prefix}/Data/efetch/{db}"
+    os.makedirs(output_dir, exist_ok=True)
+    batch_size = 5000  # You might adjust this based on your API limitations and testing
+
     credentials = read_credentials(credentialFile)
     maxWorkers = authenticate_with_entrez(credentials)
     primary_ids = read_primary_ids(json_file_path)
-    xml_records = parallel_fetch_metadata(primary_ids, db)
-    records_dict = parse_and_flatten_xml(xml_records)
-    df = pd.concat([pd.DataFrame([record]) for record in records_dict], ignore_index=True)
-    write_to_dataframe(df, output_file)
-    #### end timer
+    results = parallel_fetch_metadata(primary_ids, db, batch_size, output_dir)
+
     end_time = datetime.now()
     printAndLog(f"Script ended at {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
     elapsed_time = end_time - start_time
