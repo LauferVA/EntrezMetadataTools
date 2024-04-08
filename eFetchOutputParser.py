fb832329a91776339d048bd3c1a5fd3c8c886513
LauferVA <153957763+LauferVA@users.noreply.github.com>
2024-04-07 21:00:12 -0400

    Update eFetchOutputParser.py

--- eFetchOutputParser.py
+++ eFetchOutputParser.py
@@ -1,7 +1,7 @@
 """
 Parses output from Bio Entrez's eFetch
 This may be XML or other format - processing script needs to be flexible.
-Takes as input the files written by eFetchXmlParallel.py
+Takes as input the files written by eFetchMetadataParallel.py
 """
 
 import json
