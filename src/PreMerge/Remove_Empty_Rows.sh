06aa68af42768a4c1a4bc34f91823d5d96405435
LauferVA <153957763+LauferVA@users.noreply.github.com>
2024-04-11 23:50:16 -0400

    Update Remove_Empty_Rows.sh

--- src/PreMerge/Remove_Empty_Rows.sh
+++ src/PreMerge/Remove_Empty_Rows.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# Simple script to remove cols that are empty
+infile='${1}'
+outfile='${2}'
+
+# processes white space or tab delimted files, and will remove lines having fewer than a handful of values
+awk -F'\\s+' '(NF > 3){print $0}' ${infile} > ${outfile}
