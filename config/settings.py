dbda878d26a609b809c94ea96d6d25488ee28cae
LauferVA <153957763+LauferVA@users.noreply.github.com>
2024-04-13 12:13:43 -0400

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+# settings.py
+
+credentials = {
+    'email': 'myemail@some.place.org',
+    'apikey': 'mykey',
+    'project': 'project_name'
+}
+
+project_info = {
+    'organism': 'homo sapiens',
+    'taxid': '9606', 
+    'query': '"homo sapiens"[organism] AND RNA-Seq[All-Fields]', 
+    'org_abbrev': 'hs'
+    'db': '["biosample", "bioproject", "sra", "gds"]'
+}
