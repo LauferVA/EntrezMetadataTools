60c12510fb9d1e3787c974e075fc15008b5c94ff
LauferVA <153957763+LauferVA@users.noreply.github.com>
2024-04-13 13:13:05 -0400

    Delete libs/Remove_Empty_Rows.sh

--- libs/Remove_Empty_Rows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Simple script to remove cols that are empty
-infile='${1}'
-outfile='${2}'
-
-# processes white space or tab delimted files, and will remove lines having fewer than a handful of values
-awk -F'\\s+' '(NF > 3){print $0}' ${infile} > ${outfile}
